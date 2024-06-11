e5e9524cc433bf8c16cc677c31414e67dffc6e3f
Movin-137 <84649063+Movin-137@users.noreply.github.com>
2024-06-11 13:00:16 +0530

    Create getset.sol

---
+++ getset.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.7.0;
+
+contract GettersAndSetters{
+    //declaring variables
+    string private message;
+    uint private num;
+    bool private isTrue;
+    address private ad;
+    
+    //initializing set function
+    function setMessage(string memory newMessage,uint newNum,bool newTrue) public{
+         message=newMessage;
+         num=newNum;
+         isTrue=newTrue;
+         ad=address(this); // stores the address of the contract
+    }
+    //initializing get function
+    function getMessage() public view returns(string memory,uint,bool,address){
+          return (message,num,isTrue,ad); //returns all values as a tuple
+    }
+}
